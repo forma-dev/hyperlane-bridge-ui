8367bf114ee6256434c47a64cf4270864d942961
Huy Do <herohthd@gmail.com>
2024-04-30 21:47:35 +0700

    tab menu font bold

--- src/features/transfer/TransferTokenTab.tsx
+++ src/features/transfer/TransferTokenTab.tsx
@@ -13,7 +13,7 @@ export function TransferTokenTab({ activeTab, handleTabChange, transferType }: T
   return (
     <button onClick={() => handleTabChange(transferType)} className="w-1/2">
       <div
-        className={`hover:text-white py-7 px-20 gap-2 flex justify-center ${textClasses}`}
+        className={`hover:text-white py-7 px-20 gap-2 flex justify-center font-bold ${textClasses}`}
         style={{ borderBottom: activeTab === transferType ? '4px solid #FFFFFF' : 'none' }}
         onClick={() => handleTabChange(transferType)}
       >
