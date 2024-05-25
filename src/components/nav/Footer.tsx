743174d03853125244c2226d5a74f5d58912d47f
Josh Dechant <josh@codecrafting.org>
2024-05-25 00:10:25 -0400

    add stride

--- src/components/nav/Footer.tsx
+++ src/components/nav/Footer.tsx
@@ -13,7 +13,7 @@ export function Footer({ isSideBarOpen = false, setIsSideBarOpen }: Props) {
     >
       <div className="flex flex-col sm:flex-row gap-8 sm:gap-10 items-center justify-center">
         <div className="flex items-center">
-          <p className="text-white text-sm">Built with Hyperlane. © Cocoding Labs 2024 ✨</p>
+          <p className="text-white text-sm">Built with Hyperlane and Stride. © Cocode Labs 2024 ✨</p>
         </div>
       </div>
     </footer>
