251c339a90ab6f857af734f02a9d58a087abcc41
Josh Dechant <josh@codecrafting.org>
2024-05-27 13:48:21 -0400

    add leap snap

--- src/features/wallet/context/CosmosWalletContext.tsx
+++ src/features/wallet/context/CosmosWalletContext.tsx
@@ -5,8 +5,9 @@ import { wallets as mmWallets } from '@cosmos-kit/cosmos-extension-metamask';
 // import { wallets as cosmostationWallets } from '@cosmos-kit/cosmostation';
 // import { wallets as keplrWallets } from '@cosmos-kit/keplr';
 import { wallets as keplrWallets } from '@cosmos-kit/keplr-extension';
-// import { wallets as leapWallets } from '@cosmos-kit/leap';
+//import { wallets as leapWallets } from '@cosmos-kit/leap';
 import { wallets as leapWallets } from '@cosmos-kit/leap-extension';
+import { wallets as leapSnapWallets } from '@cosmos-kit/leap-metamask-cosmos-snap';
 import { ChainProvider } from '@cosmos-kit/react';
 import '@interchain-ui/react/styles';
 import { PropsWithChildren } from 'react';
@@ -37,6 +38,7 @@ export function CosmosWalletContext({ children }: PropsWithChildren<unknown>) {
             ...keplrWallets,
             //...cosmostationWallets,
             ...leapWallets,
+            ...leapSnapWallets,
             ...mmWallets,
           ] as MainWalletBase[]
         }
