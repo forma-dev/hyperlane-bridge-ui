aade4a9ec5a131cffebf39f72aebd98715431db9
Colin <colincooley1989@gmail.com>
2024-09-28 13:44:43 -0700

    update wagmi config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import { ToastContainer, Zoom, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { WagmiProvider } from 'wagmi';
 
 import '@hyperlane-xyz/widgets/styles.css';
 
 import { ErrorBoundary } from '../components/errors/ErrorBoundary';
 import { AppLayout } from '../components/layout/AppLayout';
+import { wagmiConfig } from '../config/wagmi';
 import { CosmosWalletContext } from '../features/wallet/context/CosmosWalletContext';
 import { EvmWalletContext } from '../features/wallet/context/EvmWalletContext';
 import { SolanaWalletContext } from '../features/wallet/context/SolanaWalletContext';
@@ -32,18 +34,20 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <ErrorBoundary>
-      <EvmWalletContext>
-        <SolanaWalletContext>
-          <CosmosWalletContext>
-            <QueryClientProvider client={reactQueryClient}>
-              <AppLayout>
-                <Component {...pageProps} />
-              </AppLayout>
-            </QueryClientProvider>
-            <ToastContainer transition={Zoom} position={toast.POSITION.BOTTOM_RIGHT} limit={2} />
-          </CosmosWalletContext>
-        </SolanaWalletContext>
-      </EvmWalletContext>
+      <WagmiProvider config={wagmiConfig}>
+        <QueryClientProvider client={reactQueryClient}>
+          <EvmWalletContext>
+            <SolanaWalletContext>
+              <CosmosWalletContext>
+                <AppLayout>
+                  <Component {...pageProps} />
+                </AppLayout>
+                <ToastContainer transition={Zoom} position={toast.POSITION.BOTTOM_RIGHT} limit={2} />
+              </CosmosWalletContext>
+            </SolanaWalletContext>
+          </EvmWalletContext>
+        </QueryClientProvider>
+      </WagmiProvider>
     </ErrorBoundary>
   );
 }
