6e2726ac694d344e73daf41d6b3b7fd28e5313cf
Josh Dechant <josh@codecrafting.org>
2024-12-20 00:47:59 -0500

    turn on relay card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { TransferTokenCard } from '../features/transfer/TransferTokenCard';
 
 const Home: NextPage = () => {
   const maintenance = false;
-  const relayEnabled = false;
+  const relayEnabled = true;
   return (
     <div className="pt-5 space-y-3">
       {maintenance ? (
